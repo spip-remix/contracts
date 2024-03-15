8ef2c3bd42b9d4ff54e01f16d427adc0044aa569
JamesRezo <james@rezo.net>
2024-03-15 12:27:49 +0100

    feat(métas): +lastModified(), +with()

--- src/MetaManagerInterface.php
+++ src/MetaManagerInterface.php
@@ -16,11 +16,26 @@ use SpipRemix\Contracts\Exception\ExceptionInterface;
 interface MetaManagerInterface extends LoggerAwareInterface
 {
     /**
+     * Instancier un MetaManager en affectant un lot de métas.
+     *
+     * @param list<array{name:non-empty-string,value:mixed,importable?:bool}> $metas
+     */
+    public static function with(array $metas = []): static;
+
+    /**
      * Démarrage du MetaManager.
      *
      * @throws ExceptionInterface Si le démarrage du MetaManager n'est pas possible.
      */
     public function boot(): void;
+
+    /**
+     * Récupérer la date de dernière mise à jour de la liste des métas,
+     * sous la forme d'un timestamp.
+     *
+     * @return positive-int|null null si le manager ne peut pas fournir de date.
+     */
+    public function lastModified(): ?int;
 
     /**
      * {@internal Pour transmettre le logger associé éventuel à un autre objet
@@ -31,7 +46,7 @@ interface MetaManagerInterface extends LoggerAwareInterface
     /**
      * Récupérer l'ensemble des métas.
      *
-     * @return list<array{name:non-empty-string,value:mixed,importable:bool}>
+     * @return list<array{name:non-empty-string,value:mixed,importable?:bool}>
      */
     public function all(): array;
 
